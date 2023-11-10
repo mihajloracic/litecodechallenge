593ec94062e283d9561b5e35e1f0abd8757001a9
Mihajlo Racic <mihajlo.racic@smartcat.io>
2023-11-10 23:06:37 +0100

    asd

--- oneTrust/src/main/java/org/example/cell/reacheable/Solution.java
+++ oneTrust/src/main/java/org/example/cell/reacheable/Solution.java
@@ -1,2 +1,11 @@
-package org.example.cell.reacheable;public class Solution {
+package org.example.cell.reacheable;
+
+public class Solution {
+
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int x_dist = Math.abs(sx - fx);
+        int y_dist = Math.abs(sy - fy);
+        int dist = Math.max(x_dist, y_dist);
+        return x_dist <= t && y_dist <= t && !(dist == 0 && t == 1);
+    }
 }
--- oneTrust/src/test/java/cell/reachable/CellReachableTest.java
+++ oneTrust/src/test/java/cell/reachable/CellReachableTest.java
@@ -1,2 +1,61 @@
-package cell.reachable;public class CellReachableTest {
+package cell.reachable;
+
+import org.example.cell.reacheable.Solution;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CellReachableTest {
+
+    @Test
+    public void simpleTestTrue() {
+        Solution solution = new Solution();
+
+        boolean reachableAtTime = solution.isReachableAtTime(1, 1, 2, 2, 1);
+
+        assertEquals(true, reachableAtTime);
+    }
+
+    @Test
+    public void simpleTestFalse() {
+        Solution solution = new Solution();
+
+        boolean reachableAtTime = solution.isReachableAtTime(1, 1, 3, 3, 1);
+
+        assertEquals(false, reachableAtTime);
+    }
+
+    @Test
+    public void leetCodeExample1() {
+        Solution solution = new Solution();
+
+        boolean reachableAtTime = solution.isReachableAtTime(2, 4, 7, 7, 6);
+
+        assertEquals(true, reachableAtTime);
+    }
+
+    @Test
+    public void leetCodeExample2() {
+        Solution solution = new Solution();
+
+        boolean reachableAtTime = solution.isReachableAtTime(3, 1, 7, 3, 3);
+
+        assertEquals(false, reachableAtTime);
+    }
+
+    @Test
+    public void leetCodeTestWhereFieldIsReachableButHasToStepOut() {
+        Solution solution = new Solution();
+        boolean reachableAtTime = solution.isReachableAtTime(1, 2, 1, 2, 1);
+
+        assertEquals(false, reachableAtTime);
+    }
+
+    @Test
+    public void leetCodeReachable() {
+        Solution solution = new Solution();
+        boolean reachableAtTime = solution.isReachableAtTime(1, 3, 2, 2, 2);
+
+        assertEquals(true, reachableAtTime);
+    }
 }
